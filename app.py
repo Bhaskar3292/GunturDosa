1e80d90dc2c591f3fe2b316c80fcaf959b66e509
bhaskar3292 <bhaskarreddy4406@gmail.com>
2024-03-01 19:09:16 -0600

    Updated the code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import itertools
 
 parser = argparse.ArgumentParser(description='Provide the Json File name')
 parser.add_argument('filename', type=str, help='Provide the filename containing json objects')
-args = parser.parse_args()
+args = parser.parse_args() 
 
 with open(args.filename, 'r') as file:
     orders_data = json.load(file)
@@ -20,15 +20,17 @@ for order in orders_data:
     customers_data[order['phone']] = order['name']
     with open(file_path, 'w') as file:
         json.dump(customers_data, file, indent=4)
-    print(f"Customer '{order['name']}' with phone number '{order['phone']}' added successfully.")
+    # print(f"Customer '{order['name']}' with phone number '{order['phone']}' added successfully.")
 
-    file_path = "items.json"
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
-            item_data = json.load(file)
-    else:
-        item_data = {}
-    for item in order['items']:
-        item_data[item['name']] = item['price']
-    with open(file_path, 'w') as file:
-        json.dump(item_data, file, indent=4)
+
+
+
+items_info = {}
+for order in orders_data:
+    for item in order["items"]:
+        item_name = item["name"]
+        items_info.setdefault(item_name, {"price": item["price"], "orders": 0})
+        items_info[item_name]["orders"] += 1
+file_path="items.json"
+with open(file_path, 'w') as file:
+    json.dump(items_info, file, indent=4)
